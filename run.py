7dc7291cb5dd1036b8e19fb6b552891591f4a3dd
1162724271@qq.com <1162724271@qq.com>
2019-11-14 22:52:16 +0800

    Add valid and summaryWriter

--- run.py
+++ run.py
@@ -1,19 +1,23 @@
 import time
 import torch
 from torch import nn as nn
+from torch.optim import Adam
+from torch.nn import MSELoss
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+
 from data.loaddata import load100KRatings, load1MRatings
 from scipy.sparse import coo_matrix
 import pandas as pd
 import numpy as np
 from numpy import diag
+from tensorboardX import SummaryWriter
+
 from graphattention.GACFmodel import GACF
-from torch.utils.data import DataLoader
-from graphattention.dataPreprosessing import ML1K
-from torch.utils.data import random_split
-from torch.optim import Adam
-from torch.nn import MSELoss
 from graphattention.GACFmodel import SVD
 from graphattention.GACFmodel import NCF
+from graphattention.dataPreprosessing import ML1K
+
 
 # import os
 # os.chdir('/home/chenliu/DeepGraphFramework/NeuralCollaborativeFiltering/NGCF-pytorch')
@@ -49,43 +53,72 @@ rt['itemId'] = rt['itemId'] - 1
 # L = np.dot(np.dot(D_,mat),D_)
 #
 para = {
-    'epoch':60,
+    'epoch':30,
     'lr':0.01,
-    'batch_size':256,
-    'train':0.8
+    'weight_decay': 0.001,
+    'batch_size':2048,
+    'train':0.7,
+    'valid':0.15
 }
 
 ds = ML1K(rt)
 trainLen = int(para['train']*len(ds))
-train,test = random_split(ds,[trainLen,len(ds)-trainLen])
-dl = DataLoader(train,batch_size=para['batch_size'],shuffle=True,pin_memory=True)
+validLen = int(para['valid']*len(ds))
+train, valid, test = random_split(ds,[trainLen, validLen, len(ds)- validLen -trainLen])
 
-model = GACF(userNum, itemNum, rt, 128, layers=[128,64,]).cuda()
+train_loader = DataLoader(train, batch_size=para['batch_size'], shuffle=True,pin_memory=True)
+valid_loader = DataLoader(valid, batch_size=len(valid), shuffle=False,pin_memory=True)
+
+model = GACF(userNum, itemNum, rt, 128, layers=[128,128,128]).cuda()
 # model = SVD(userNum,itemNum,50).cuda()
 # model = NCF(userNum,itemNum,64,layers=[128,64,32,16,8]).cuda()
-optim = Adam(model.parameters(), lr=para['lr'],weight_decay=0.001)
+optim = Adam(model.parameters(), lr=para['lr'],weight_decay=para['weight_decay'])
 lossfn = MSELoss()
 
-for i in range(para['epoch']):
-    epoch_loss = 0.0
-    start_time = time.time()
-    for id,batch in enumerate(dl):
-        # print(id, len(dl))
+def train(model, train_loader, optim, lossfn):
+    model.train()
+    total_loss = 0.0
+    for _, batch in enumerate(train_loader):
         optim.zero_grad()
         prediction = model(batch[0].cuda(), batch[1].cuda())
         loss = lossfn(batch[2].float().cuda(),prediction)
         loss.backward()
         optim.step()
-        epoch_loss += loss.item()
-        # print(loss.item())
-    print("The time elapse of epoch {:03d}".format(i) + " is: " + time.strftime("%H: %M: %S", time.gmtime(time.time() - start_time)))
-    print('epoch:{},loss:{}'.format(i, epoch_loss/id))
+        total_loss += loss.item()
+    return total_loss/len(train_loader)
+
+def valid(model, valid_loader, lossfn):
+    model.eval()
+    total_loss = 0.0
+    for _, batch in enumerate(valid_loader):
+        prediction = model(batch[0].cuda(), batch[1].cuda())
+        loss = lossfn(batch[2].float().cuda(),prediction)
+        total_loss += loss.item()
+    return total_loss/len(valid_loader)
 
 
-testdl = DataLoader(test,batch_size=len(test),)
-for data in testdl:
-    prediction = model(data[0].cuda(),data[1].cuda())
+# Add summaryWriter. Results are in ./runs/. Run 'tensorboard --logdir=./runs' and see in browser.
+summaryWriter = SummaryWriter(comment='lr:{}wd:{}'.format(para['lr'], para['weight_decay']))
 
-loss = lossfn(data[2].float().cuda(),prediction)
-print('testloss:', loss.item()) # MSEloss
+for epoch in range(para['epoch']):
+    epoch_loss = 0.0
+    start_time = time.time()
+    train_loss = train(model, train_loader, optim, lossfn)
+    valid_loss = valid(model, valid_loader, lossfn)
+    summaryWriter.add_scalar('loss/train_loss', train_loss, epoch)
+    summaryWriter.add_scalar('loss/valid_loss', valid_loss, epoch)
+    print("The time elapse of epoch {:03d}".format(epoch) + " is: " + time.strftime("%H: %M: %S", time.gmtime(time.time() - start_time)))
+    print('epoch:{}, train_loss:{}, valid_loss:{}'.format(epoch, train_loss, valid_loss))
+
+
+test_loader = DataLoader(test,batch_size=len(test),)
+test_loss = valid(model, test_loader, lossfn)
+print('test_loss:', test_loss)
+
+
+# for data in testdl:
+#     prediction = model(data[0].cuda(),data[1].cuda())
+
+# loss = lossfn(data[2].float().cuda(),prediction)
+# print('testloss:', loss.item()) # MSEloss
 
