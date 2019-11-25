53162021387b4e6150cf6c8e055592a2905922dd
1162724271@qq.com <1162724271@qq.com>
2019-11-25 16:58:14 +0800

    Add GACF5_BPR

--- graphattention/GACFmodel5.py
+++ graphattention/GACFmodel5.py
@@ -41,9 +41,86 @@ class GALayer(Module):
         feature2 = self.affine2(torch.sparse.mm(L2, features))
         return feature1 + feature2
 
+class GACFV5_BPR(Module):
+
+    def __init__(self,userNum,itemNum,adj,embedSize=256,layers=[256,128,64], droprate=0.2, useCuda=True):
+
+        super(GACFV5_BPR, self).__init__()
+        self.useCuda = useCuda
+        self.userNum = userNum
+        self.itemNum = itemNum
+        self.droprate = droprate
+        self.uEmbd = nn.Embedding(userNum,embedSize)
+        self.iEmbd = nn.Embedding(itemNum,embedSize)
+        
+        self.GALayers = torch.nn.ModuleList()        
+        self.LaplacianMat = adj 
+        self.selfLoop = self.getSparseEye(self.userNum+self.itemNum)
+
+        for From,To in zip(layers[:-1],layers[1:]):
+            self.GALayers.append(GALayer(From, To, 8, self.droprate))
+        
+        self._init_weight_()
+
+    def _init_weight_(self):
+        nn.init.normal_(self.uEmbd.weight, std=0.01)
+        nn.init.normal_(self.iEmbd.weight, std=0.01)
+
+    def getFeatureMat(self):
+        uidx = torch.LongTensor([i for i in range(self.userNum)])
+        iidx = torch.LongTensor([i for i in range(self.itemNum)])
+        if self.useCuda == True:
+            uidx = uidx.cuda()
+            iidx = iidx.cuda()
+
+        userEmbd = self.uEmbd(uidx)
+        itemEmbd = self.iEmbd(iidx)
+        features = torch.cat([userEmbd,itemEmbd],dim=0)
+        return uidx, iidx + self.userNum , features
+       
+    def forward(self, userIdx, itemIdx):
+        if self.training:
+            # gcf data propagation
+            uidx, iidx, features = self.getFeatureMat()
+            
+            finalEmbd = features.clone()
+
+            for ga in self.GALayers:
+
+                userFeatures, itemFeatures = features[uidx], features[iidx]
+                features = ga(userFeatures, itemFeatures, self.LaplacianMat, self.selfLoop)
+
+                finalEmbd = torch.cat([finalEmbd, features.clone()],dim=1)
+
+            itemIdx = itemIdx + self.userNum
+            userIdx = list(userIdx.cpu().data)
+            itemIdx = list(itemIdx.cpu().data)
+
+            userEmbd = finalEmbd[userIdx]
+            itemEmbd = finalEmbd[itemIdx]
+            embd = torch.cat([userEmbd,itemEmbd],dim=1)
+
+            embd = nn.ReLU()(self.transForm1(embd))
+            embd = nn.ReLU()(self.transForm2(embd))
+            embd = self.transForm3(embd)
+            prediction = embd.flatten()
+
+            return prediction  
+        else:
+            itemIdx = itemIdx + self.userNum
+            userEmbd = self.finalEmbd[userIdx]
+            itemEmbd = self.finalEmbd[itemIdx]
+            return torch.mm(userEmbd, itemEmbd.transpose(1, 0))       
+        
+
+    def getSparseEye(self,num):
+        i = torch.LongTensor([[k for k in range(0,num)],[j for j in range(0,num)]])
+        val = torch.FloatTensor([1]*num)
+        return torch.sparse.FloatTensor(i,val)
+
 class GACFV5(Module):
 
-    def __init__(self,userNum,itemNum,rt,embedSize=256,layers=[256,128,64], droprate=0.2, useCuda=True):
+    def __init__(self,userNum,itemNum,adj,embedSize=256,layers=[256,128,64], droprate=0.2, useCuda=True):
 
         super(GACFV5, self).__init__()
         self.useCuda = useCuda
@@ -53,11 +130,9 @@ class GACFV5(Module):
         self.uEmbd = nn.Embedding(userNum,embedSize)
         self.iEmbd = nn.Embedding(itemNum,embedSize)
         
-        self.GALayers = torch.nn.ModuleList()
-        
-                
-        self.LaplacianMat = self.buildLaplacianMat(rt) # sparse format
-        self.leakyRelu = nn.LeakyReLU()
+        self.GALayers = torch.nn.ModuleList()      
+        # self.LaplacianMat = self.buildLaplacianMat(rt) # sparse format
+        self.LaplacianMat = adj
         self.selfLoop = self.getSparseEye(self.userNum+self.itemNum)
 
         self.transForm1 = nn.Linear(in_features=sum(layers)*2,out_features=64)
@@ -96,7 +171,7 @@ class GACFV5(Module):
         return uidx, iidx + self.userNum , features
        
     def forward(self, userIdx, itemIdx):
-
+        
         # gcf data propagation
         uidx, iidx, features = self.getFeatureMat()
         
@@ -124,32 +199,9 @@ class GACFV5(Module):
 
         return prediction        
         
+        
 
     def getSparseEye(self,num):
         i = torch.LongTensor([[k for k in range(0,num)],[j for j in range(0,num)]])
         val = torch.FloatTensor([1]*num)
         return torch.sparse.FloatTensor(i,val)
-
-    def buildLaplacianMat(self,rt):
-
-        rt_item = rt['itemId'] + self.userNum
-        uiMat = coo_matrix((rt['rating'], (rt['userId'], rt['itemId'])))
-
-        uiMat_upperPart = coo_matrix((rt['rating'], (rt['userId'], rt_item)))
-        uiMat = uiMat.transpose()
-        uiMat.resize((self.itemNum, self.userNum + self.itemNum))
-
-        A = sparse.vstack([uiMat_upperPart,uiMat])
-        selfLoop = sparse.eye(self.userNum+self.itemNum)
-        sumArr = (A>0).sum(axis=1)
-        diag = list(np.array(sumArr.flatten())[0])
-        diag = np.power(diag,-0.5)
-        D = sparse.diags(diag)
-        L = D * A * D
-        L = sparse.coo_matrix(L)
-        row = L.row
-        col = L.col
-        i = torch.LongTensor([row,col])
-        data = torch.FloatTensor(L.data)
-        SparseL = torch.sparse.FloatTensor(i,data)
-        return SparseL
